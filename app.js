70ab071997a01c099125a85379b8733eac8281c5
Prasad <154584@NTTDATA.COM>
2021-01-10 00:28:45 +0530

    fix

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 const cookieParser = require("cookie-parser");
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const dotenv = require("dotenv");
 
 const app = express();
