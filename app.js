c1ebee75e2455ad44581920d7c9e81a24cd18cfe
Prasad <154584@NTTDATA.COM>
2021-01-10 00:08:51 +0530

    heroku deploy fix

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes');
-const cookieParser = require('cookie-parser');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
 
 const app = express();
 dotenv.config();
@@ -12,19 +12,22 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.send("altrimetrik-code-assignment-server login and registration node api");
+  res.send(
+    "altrimetrik-code-assignment-server login and registration node api"
+  );
 });
 
 // database connection
 const dbURI = process.env.CONNECTION_URL;
 const port = process.env.PORT || 3000;
-// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-//   .then((result) => app.listen(port))
-//   .catch((err) => console.log(err));
-
-app.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(port))
+  .catch((err) => console.log(err));
 
 // routes
 app.use(authRoutes);
